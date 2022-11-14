4c44eae943da4a2d0c08b121d06c65de6f51f68c
Ilya Markov <imarkovkin@gmail.com>
2022-11-15 00:55:08 +0300

    Добавьте файлы проекта.

---
+++ Task 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = Console.ReadLine();
+            string str2 = str1.ToLower();
+            string str3 = str2.Replace(" ", "");
+            string str4 = "";
+            foreach (char c in str3)
+            {
+                str4 = c + str4;
+            }
+            if (str4 == str3)
+            {
+                Console.WriteLine("Фраза является палиндромом");
+            }
+            else
+            {
+                Console.WriteLine("Фраза не является палиндромом");
+            }
+            Console.ReadKey();
+        }
+    }
+}
